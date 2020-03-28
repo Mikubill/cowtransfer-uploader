dc7e664cab179e4a87d4a69472c1f3463d0cc662
Miyako Satori <31246794+Mikubill@users.noreply.github.com>
2020-03-29 01:55:10 +0800

    add: install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+hash tar uname grep curl head
+OS="`uname`"
+case $OS in
+  Linux)
+    OS='linux'
+    ;;
+  FreeBSD)
+    OS='freebsd'
+    ;;
+  Darwin)
+    OS='darwin'
+    ;;
+  *)
+    echo 'OS not supported'
+    exit 2
+    ;;
+esac
+
+ARCH="`uname -m`"
+case $ARCH in
+  x86_64|amd64)
+    ARCH='amd64'
+    ;;
+  aarch64)
+    ARCH='arm64'
+    ;;
+  *)
+    echo 'OS type not supported'
+    exit 2
+    ;;
+esac
+
+VERSION=$(curl -s https://api.github.com/repos/Mikubill/cowtransfer-uploader/releases/latest 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
+
+curl -sL https://github.com/Mikubill/cowtransfer-uploader/releases/download/v$VERSION/cowtransfer-uploader_$VERSION\_$OS\_$ARCH.tar.gz | tar xz
+
+printf "\nCowTransfer-uploader Downloded.\n\n"
+exit 0
